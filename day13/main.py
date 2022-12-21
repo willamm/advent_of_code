1ae92905784b2c2a7b23c9a3765bca45c0b4d02e
William Murphy <will.murphy1994@gmail.com>
2022-12-20 17:05:49 -0800

    Day 13 part 2 in progress

--- day13/main.py
+++ day13/main.py
@@ -1,17 +1,17 @@
 from ast import literal_eval
 from itertools import zip_longest
+from functools import cmp_to_key
+from math import prod
 
 def parse(filename):
-    packet_pairs = [[]]
+    packet_pairs = []
     with open(filename, "r") as f:
-        data = f.read().strip().splitlines()
-        i = 0
-        for line in data:
-            if len(line) == 0:
-                packet_pairs.append([])
-                i += 1
-                continue
-            packet_pairs[i].append(literal_eval(line))
+        data = f.read().strip()
+        for i, p in enumerate(data.split("\n\n")):
+            packet_pairs.append([])
+            for k in p.split("\n"):
+                packet_pairs[i].append(literal_eval(k))
+                
     return packet_pairs
 
 
@@ -39,15 +39,24 @@ def process_pairs(left, right):
     return process_pairs(l, r)
 
 def main():
-    data = parse("input.txt")
-    pairs_in_right_order = set()
-    sum = 0
-    for x, pair in enumerate(data):
-        # Compare each pair recursively
-        left = pair[0]
-        right = pair[1]
-        if process_pairs(left, right) == -1:
-            sum += x 
-    print(sum)
+    #data = parse("input.txt")
+    #sum = 0
+    #for x, pair in enumerate(data):
+        ## Compare each pair recursively
+        #left = pair[0]
+        #right = pair[1]
+        #if process_pairs(left, right) == -1:
+            #sum += x 
+    #print(sum)
+    pairs = parse("input.txt")
+   # data = open("input.txt").read().strip()
+    
+    #pairs = [list(map(literal_eval, p.split("\n"))) for p in data.split("\n\n")]
+    print(sum(i for i, (l, r) in enumerate(pairs, 1) if process_pairs(l, r) == -1))
+
+    # part 2
+    # doesn't work yet
+    packets = sorted([y for x in pairs for y in x] + [[[2]], [[6]]], key=cmp_to_key(process_pairs), reverse=True)
+    print(prod([n for n, packet in enumerate(packets, 1) if packet in ([[2]], [[6]])]))
 
 main()
